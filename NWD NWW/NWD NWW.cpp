961dce28614a1970dc67bf9e5a97782cdb3c1d65
Jakub Klimaszewski (Uczeń) <jklimaszewski144@technikumkreatywne.pl>
2022-04-07 09:57:51 +0200

    dodano zadanie NWD NWW

---
+++ NWD NWW/NWD NWW.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int nww;
+	int iloczyn;
+
+	cout << "Podaj pierwsza liczbe:" << endl;
+	cin >> a;
+	cout << "Podaj druga liczbe:" << endl;
+	cin >> b;
+	iloczyn = a * b;
+	while (a != b) {
+		if (a < b)
+			b = b - a;
+		else
+			a = a - b;
+	}
+
+	nww = iloczyn / a;
+	cout << "Najwiekszy wspolny dzielnik: " << a << endl;
+	cout << "Najwieksza wspolna wielokrotnosc: " << nww << endl;
+
+
+
+
+
+}
