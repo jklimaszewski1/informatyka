bd6f35a7ab701646bf4c6d661c3086c50307aae2
jklimaszewski1 <iwaz73@wp.pl>
2022-05-27 02:43:07 +0200

    Dodano zadanie "Ciąg Fibonacciego"

---
+++ Ciąg Fibonacciego/Fibonacci.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+
+void Ciag(int n);
+
+int main()
+{
+	int n;
+
+	cout << "Ile wyrazow ciagu wypisac:" << endl;
+	cin >> n;
+	cout << endl;
+
+	Ciag(n);
+
+	return 0;
+}
+
+void Ciag(int n)
+{
+	int f0 = 0;
+	int f1 = 1;
+	int f; 
+
+	for (int i = 0; i <= n; i++) {
+		
+		if (i > 1) {
+			f = f0 + f1;
+			f0 = f1;
+			f1 = f;
+		}
+		else {
+			f = i;
+		}
+		cout << f << endl;
+		
+	}
+}
