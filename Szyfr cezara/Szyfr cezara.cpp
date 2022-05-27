d36dfbd7f3382b85f1f3dadf9e83028431297755
jklimaszewski1 <iwaz73@wp.pl>
2022-05-27 03:19:12 +0200

    Poprawiono "Szyfr Cezara"

--- Szyfr cezara/Szyfr cezara.cpp
+++ Szyfr cezara/Szyfr cezara.cpp
@@ -14,24 +14,24 @@ int main()
 
 	cout << "Podaj klucz z przedialu [-26..26]:" << endl;
 	cin >> klucz;
-	
+
 	Szyfr(tab, klucz);
 	cout << "Po zaszyfrowaniu: " << tab << endl;
 	Szyfr(tab, -klucz);
 	cout << "Po rozszyfrowaniu: " << tab << endl;
 }
 
-void Szyfr(char tab[], int klucz) 
+void Szyfr(char tab[], int klucz)
 {
 	int dl;
 
-	dl = sizeof(tab);
-	
-	if (!(klucz >= -26 and klucz <= 26)){
+	dl = strlen(tab);
+
+	if (!(klucz >= -26 and klucz <= 26)) {
 		return;
 	}
-	
-	if (klucz >= 0) {	
+
+	if (klucz >= 0) {
 		for (int i = 0; i < dl; i++) {
 			if (tab[i] + klucz <= 'z') {
 				tab[i] += klucz;
@@ -47,9 +47,9 @@ void Szyfr(char tab[], int klucz)
 				tab[i] += klucz;
 			}
 			else {
-			tab[i] = tab[i] + klucz + 26;
+				tab[i] = tab[i] + klucz + 26;
 			}
-		
+
 		}
 	}
 }
